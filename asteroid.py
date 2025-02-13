010979dd4581b49d3a5f70f0cf87162e3e1b4f23
ch0ppah <nick0suna76@gmail.com>
2025-02-13 01:33:04 -0500

    add player movement

---
+++ asteroid.py
@@ -0,0 +1,8 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x, y, radius):
+        
--- main.py
+++ main.py
@@ -1,28 +1,39 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
-from player import Player
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    running = True
-    player = Player(SCREEN_WIDTH /2 , SCREEN_HEIGHT / 2)
 
-    while running:
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
