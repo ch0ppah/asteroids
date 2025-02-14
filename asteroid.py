328148e9ceb93eef9b7384c67827b4c43a9ce436
ch0ppah <nick0suna76@gmail.com>
2025-02-13 22:56:51 -0500

    add asteroid split and finish project requirements

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from constants import *
 from circleshape import CircleShape
 
 
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            random_angle = random.uniform(20, 50)
+            asteroid_one_vector = self.velocity.rotate(random_angle)
+            asteroid_two_vector = self.velocity.rotate(-random_angle)
+            new_asteroids_radius = self.radius - ASTEROID_MIN_RADIUS
+            first_new_asteroid = Asteroid(self.position.x, self.position.y, new_asteroids_radius)
+            second_new_asteroid = Asteroid(self.position.x, self.position.y, new_asteroids_radius)
+            first_new_asteroid.velocity = asteroid_one_vector * 1.2
+            second_new_asteroid.velocity = asteroid_two_vector * 1.2
--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         pygame.display.flip()
